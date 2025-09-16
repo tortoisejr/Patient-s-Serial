804e3a97fbd6b322be82f3f6575a031902707b71
tortoisejr <mdsabbirh083@gmail.com>
2025-09-16 19:57:49 +0600

    my third commit

--- contructs/patientserial.sol
+++ contructs/patientserial.sol
@@ -3,6 +3,12 @@ pragma solidity 0.8.19;
 
 contract myContract{
 
+    struct Serial{
+        uint serialNo;
+        uint patientId;
+        bool sarved;
+    }
+
     struct Patient
     {
         uint Date;
@@ -30,7 +36,9 @@ contract myContract{
     }
 mapping(uint =>Doctor) public doctors;
 mapping(uint => Patient) public patients;
-mapping(uint =>Patient) public PatientsSerial;
+mapping(uint =>Serial) public PatientsSerial;
+uint head=1;
+uint tail=1;
 uint public doctorCount=0;
 uint public patientCount=0;
 uint public specialPatientCount=0;
@@ -58,8 +66,47 @@ modifier ispaid(uint _patientId)
     _;
 }
 
+event addpatient(uint Date,
+        uint patientId,
+        string name,
+        uint age,
+        string syndrome,
+        uint serialNo,
+        uint roomNo,
+        uint doctorId,
+        bool paymentStatus, 
+        bool special,
+        bool sarved);
+
+event adddoctor(uint doctorId,
+        string name,
+        string digree,
+        string Spaciality,
+        uint dutyStartTime,
+        uint dutyEndTime,
+        bool Availablity,
+        uint roomNo);
 
 
+function addSerial(uint _date,string memory  _name,uint _age,string memory _syndrome,uint _roomNo, uint _doctorId,bool _paymentStatus,bool _special) public doctorExistency(_doctorId) doctorAvailablity(_doctorId)
+{
+    Patient storage patient = patients[++patientCount];
+
+    patient.Date= block.timestamp+_date;
+    patient.patientId = patientCount;
+    patient.name= _name;
+    patient.age=_age;
+    patient.syndrome=_syndrome;
+    patient.serialNo=++serialCount;
+    patient.roomNo=_roomNo;
+    patient.doctorId=_doctorId;
+    patient.paymentStatus=_paymentStatus;
+    patient.special=_special;
+    patient.sarved=false;
+
+    emit addpatient(patient.Date, patient.patientId, patient.name, patient.age, patient.syndrome, patient.serialNo, patient.roomNo, patient.doctorId, patient.paymentStatus, patient.special, patient.sarved);
+
+}
 
 
 
